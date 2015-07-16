95cc23ae6d1f09c443b695bd4e7b4908140deef5
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-07-16 14:08:02 -0600

    draw boxes

--- demos/canvas-blocks/script.js
+++ demos/canvas-blocks/script.js
@@ -1,3 +1,24 @@
 var canvas = document.getElementById('game');
 var context = canvas.getContext('2d');
+var x = 0;
+var width = 50;
+
+function Block(x, y, width, height) {
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+};
+
+Block.prototype.draw = function() {
+  context.fillRect();
+};
+
+function gameLoop() {
+  if(x < canvas.width - width) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  };
+};
+
+gameLoop();
 
